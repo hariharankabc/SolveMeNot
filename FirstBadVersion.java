352efc88dca50f2ab8475e8a8c682bbf9719db3d
HAK <67498648+hariharankabc@users.noreply.github.com>
2022-04-21 11:16:37 +0530

    [Problem solving]: FirstBadVersion

---
+++ FirstBadVersion.java
@@ -0,0 +1,12 @@
+// 278. problems/first-bad-version/
+public class FirstBadVersion extends VersionControl {
+    public int firstBadVersion(int n) {
+        int start=1, end=n;
+        while(start < end) {
+            int mid = start + (end-start) / 2;
+            if(isBadVersion(mid)) end = mid;
+            else start = mid + 1;
+        }
+        return start;
+    }
+}
