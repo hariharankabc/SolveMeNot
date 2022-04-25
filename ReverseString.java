acdb2591284aa7e118b735285bfadfd08dc21854
HAK <67498648+hariharankabc@users.noreply.github.com>
2022-04-25 09:40:04 +0530

    [Problem Solving]: ReverseString

---
+++ ReverseString.java
@@ -0,0 +1,10 @@
+class ReverseString {
+// 344.problems/reverse-string/
+    public void reverseString(char[] s) {
+        for(int i=0,j=s.length-1; i<j; i++,j--){
+            char tempChar = s[i];
+            s[i] = s[j];
+            s[j] = tempChar;
+        }
+    }
+}
