f74f66981bc20adf3ee22369552faa027dba535a
HAK <67498648+hariharankabc@users.noreply.github.com>
2022-05-03 17:51:28 +0530

    [Problem Solving]: LongestSubstringWithoutRepeatingCharacters

---
+++ LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,16 @@
+class LongestSubstringWithoutRepeatingCharacters {
+// 3.problems/longest-substring-without-repeating-characters [NB]
+    public int lengthOfLongestSubstring(String s) {
+        HashMap<Character, Integer> mem = new HashMap<>();
+        int l=0, max=0;
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            if(mem.containsKey(c)){
+                l=Math.max(l,mem.get(c)+1);
+            }
+            mem.put(c,i);
+            max = Math.max(max, (i-l+1));
+        }
+        return max;
+    }
+}
