2331a5723a5021073030250fb1c30315ece3ff1e
HAK <67498648+hariharankabc@users.noreply.github.com>
2022-06-07 11:08:54 +0530

    [Problem Solving]: FindMinimumInRotatedSortedArray

---
+++ FindMinimumInRotatedSortedArray.java
@@ -0,0 +1,15 @@
+class FindMinimumInRotatedSortedArray {
+// 153. problems/find-minimum-in-rotated-sorted-array
+    public int findMin(int[] nums) {
+        int low= 0 ;
+        int high  =  nums.length-1;
+        while(high>low){
+            int mid  =  low+ (high- low)/2;
+            if(nums[mid]> nums[high])
+                 low =  mid+1;
+            else 
+                high  =  mid;
+        }
+        return nums[low];
+    }
+}
